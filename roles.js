e0a6b116f72e0c038bb0ee6ad9b16541ada9924d
azcoigreach <azcoigreach@gmail.com>
2017-11-25 16:23:33 -0700

    tweak upgrader

--- roles.js
+++ roles.js
@@ -120,7 +120,7 @@ module.exports = {
 				creep.memory.task = creep.memory.task || creep.getTask_Withdraw_Storage("energy", 
 				_.get(Memory, ["rooms", creep.room.name, "survey", "downgrade_critical"], true));
 				creep.memory.task = creep.memory.task || creep.getTask_Withdraw_Container("energy", 
-				_.get(Memory, ["rooms", creep.room.name, "survey", "downgrade_critical"], true));
+				_.get(Memory, ["rooms", creep.room.name, "survey", "downgrade_critical"], false));
 				creep.memory.task = creep.memory.task || creep.getTask_Wait(10);
 
 				creep.runTask(creep);
